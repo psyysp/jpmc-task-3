e0692e4ff70115e46a0296789c75df3ed1a6c94c
psy <luckyluckycopper@gmail.com>
2022-10-22 21:46:02 -0400

    updated ui/ux to track and display price ratio between two stocks and trigger an alert when the ratio passes an upper or lower bound

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,38 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    // calculating the price of each stock and their ratio
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+
+    // left the bounds as 5% to better display the trigger_alert functionality
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+    return {
+      // returning the necessary values
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+        serverRespond[0].timestamp : serverRespond[1].timestamp, // ensures that the timestamps return appropriately
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+
+      // ensures that the trigger_alert works when the ratio crosses the upper or lower bounds
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined
+    };
   }
 }
